47d48033509700f04290a682bf2a45c3d70677c0
Markus Bruecklmayr <markus@bruecklmayr.de>
2024-07-22 12:13:50 +0200

    integrated Krzysztof comments

--- src/localdataset/dtypes.py
+++ src/localdataset/dtypes.py
@@ -4,12 +4,13 @@ Module contains data types used by the LocalDataManagement tool
 
 from pathlib import Path
 from typing import List, NamedTuple
+from uuid import UUID
 
 # Types
 INDICES = List[int]
 LABEL = str
 INDEX = int
-GID = str
+GID = UUID
 TAGS = List[int]
 
 
